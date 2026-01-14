047dda328719c48ca97707b99a765ea0e36df858
wit <chaithongsorawit61@gmail.com>
2026-01-14 17:08:26 +0700

    update

--- Lab1_wit.c
+++ Lab1_wit.c
@@ -3,14 +3,27 @@
 // Kittiphat Noikate 67070503459
 #include <stdio.h>
 
-int input(){
+void two_com(int bit, long long int binary){
+    int arr[bit];
+    for(int i=bit-1; i>=0; i--){
+        arr[i] = binary % 10;
+        binary /= 10;
+    }
+
+    //test print
+    for(int i=0; i<bit; i++){
+        printf("%d", arr[i]);
+    }
+}
+
+void input(){
     int bit;
     long long int decimal;
     printf("Please enter the number of bits between 1 and 32 inclusive: ");
-    scanf("%d\n", &bit);
+    scanf("%d", &bit);
     printf("Please enter a number between -4 and 3 inclusive: ");
-    scanf("%lld\n", &decimal);
-    return (num,value);
+    scanf("%lld", &decimal);
+    two_com(bit, decimal);
 }
 
 int main() {
