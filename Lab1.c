dd716303d28a926409725dc95f5d53394188628a
MooDengHippo <jadet26368@gmail.com>
2026-01-14 18:06:02 +0700

    Update Lab1.c

--- Lab1.c
+++ Lab1.c
@@ -4,39 +4,33 @@
 
 #include <stdio.h>
 
-// =====================================================
-// SHARED UTILITY FUNCTIONS (for all representations)
-// =====================================================
+int abs(int value){
 
-int power_2(int n){
+  if(value < 0){
 
-  int result = 1;
-  int i;
+    return -value;
 
-  for (i = 0; i < n; i++){
-    result = result * 2;
   }
 
-  return result;
+  return value;
 
 }
 
-int abs(int value){
+int power_2(int n){
 
-  if (value < 0) {
-    return -value;
-  }
-  return value;
+  return 1 << n;
 
 }
 
 void binary(int val, int n){
 
   int bit;
+
   for(bit = n - 1; bit >= 0; bit--){
 
     int bit_val = power_2(bit);
-    if(val >= bit_val){
+
+    if(val >= bit_val) {
 
       printf("1");
       val = val - bit_val;
@@ -46,14 +40,12 @@ void binary(int val, int n){
       printf("0");
 
     }
-  }
-}
 
-// =====================================================
-// SIGN-MAGNITUDE REPRESENTATION
-// =====================================================
-// Range: -(2^(n-1) - 1) to +(2^(n-1) - 1)
-// Note: Has two representations for zero (+0 and -0)
+  }
+
+  printf(".");
+
+}
 
 void sign_and_magnitude(int n, int val){
 
@@ -62,12 +54,15 @@ void sign_and_magnitude(int n, int val){
 
   if(abs_val <= MSB){
 
-    printf("The sign-magnitude does represent %d.\n", val);
+    printf("The sign-magnitude representation of %d is ", val);
 
     if(val < 0){
+
       printf("1");
     }else{
+
       printf("0");
+
     }
 
     binary(abs_val, n - 1);
@@ -76,5 +71,53 @@ void sign_and_magnitude(int n, int val){
   }else{
 
     printf("The sign-magnitude doesn't represent %d.\n", val);
+
   }
+
 }
+
+int main(){
+
+  sign_and_magnitude(3, -4);
+  sign_and_magnitude(4, -7);
+  sign_and_magnitude(3, 2);
+  sign_and_magnitude(4, 0);
+  sign_and_magnitude(4, -0);
+  sign_and_magnitude(8, 64);
+  sign_and_magnitude(3, 3);
+
+  return 0;
+
+}
+
+// void two_com(int bit, long long int decimal){
+//     int arr[bit] = {0};
+//     int dec;
+
+//     if (decimal < 0){
+//         dec = abs(decimal);
+//     }else{
+//         dec = decimal;
+//     }
+
+//     for (int i = bit-1; i>=0; i--){
+//         arr[i] = dec%2;
+//         dec /= 2;
+//     }
+// }
+
+// void input(){
+//     int bit;
+//     long long int decimal;
+//     printf("Please enter the number of bits between 1 and 32 inclusive: ");
+//     scanf("%d", &bit);
+//     printf("Please enter a number between -4 and 3 inclusive: ");
+//     scanf("%lld", &decimal);
+//     binary(decimal, bit);
+//     two_com()
+// }
+
+// int main() {
+//     input();
+//   return 0;
+// }
