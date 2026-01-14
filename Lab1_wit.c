be63fc724ed070ed21b69c4ae816abc7e3e6bbc3
wit <chaithongsorawit61@gmail.com>
2026-01-14 16:17:17 +0700

    fix

--- Lab1_wit.c
+++ Lab1_wit.c
@@ -3,3 +3,12 @@
 // Kittiphat Noikate 67070503459
 #include <stdio.h>
 
+int input(int num,long long int value){
+    int bit = num;
+    long long int decimal = value;
+    return (num,value);
+}
+
+int main() {
+  return 0;
+}
