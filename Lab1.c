2e1ecc3a07dd2e9aaacd6adaba752b72ca1a6358
wit <chaithongsorawit61@gmail.com>
2026-01-14 16:16:15 +0700

    Update Lab1 file

--- Lab1.c
+++
@@ -1,2 +0,0 @@
-// Sorawit Chaithong 67070503442
-// Kittiphat Noikate 67070503459
---
+++ Lab1_wit.c
@@ -0,0 +1,5 @@
+// Punjapong Poobanchuen 67070503423
+// Sorawit Chaithong 67070503442
+// Kittiphat Noikate 67070503459
+#include <stdio.h>
+
